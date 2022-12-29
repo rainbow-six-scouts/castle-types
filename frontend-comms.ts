90dc3319ed1098f141f251fd3e54df624cc37763
MierenManz <christiaanmens@gmail.com>
2022-12-29 19:20:38 +0100

    x

---
+++ frontend-comms.ts
@@ -0,0 +1,49 @@
+/**
+ * All events prefixed with `Server` are server => client
+ * All events prefixed with `Client` are client => server
+ */
+
+interface Connect {
+  kind: "connect";
+  username: string;
+  team: 0 | 1;
+}
+// Removed `username` from `Connect`
+export type ClientConnect = Omit<Connect, "username">;
+export type ServerConnect = Connect;
+
+interface Disconnect {
+  kind: "disconnect";
+  reason: string;
+}
+export type ClientDisconnect = Disconnect;
+export type ServerDisconnect = Disconnect;
+
+interface ChangeTeams {
+  kind: "change-teams";
+  username: string;
+  team: 0 | 1;
+}
+// Removed `username` and `team` from `ChangeTeams`
+export type ClientChangeTeams = Omit<ChangeTeams, "username" | "team">;
+export type ServerChangeTeams = ChangeTeams;
+
+interface RingTeam {
+  kind: "ring-team";
+  team: 0 | 1;
+}
+// Removed `team` from `RingTeam`
+export type ClientRingTeam = Omit<RingTeam, "team">;
+export type ServerRingTeam = RingTeam;
+
+/**
+ *  These events are only send from the server
+ */
+
+export interface ServerGameStart {
+  kind: "game-start";
+}
+
+export interface ServerGameOver {
+  kind: "game-over";
+}
